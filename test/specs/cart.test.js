3b9dd2b89b7a5d236964537914455d8824d76890
Aditya kumar jha <adityaceo007@gmail.com>
2026-02-27 18:42:22 +0530

    fix: Add .js extensions to all require imports

--- test/specs/cart.test.js
+++ test/specs/cart.test.js
@@ -1,5 +1,5 @@
 const LoginPage = require('../pageobjects/login.page.js')
-const ProductsPage = require('../pageobjects/product.page.jsgit push -u origin main')
+const ProductsPage = require('../pageobjects/product.page.js')
 
 describe('Cart Feature', () => {
 
